3cf20790b87c26f5917ba72475b4f0984bcb5f1b
UMA CATARINA GOTFRYD <47805869@ort>
2024-09-12 10:28:17 -0300

    Finallll

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,6 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -13,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
   
     <html lang="en">
-      <body className={inter.className}>
+      <body>
       <nav>
         <ul>
         <li><Link href="/">Home</Link></li>
